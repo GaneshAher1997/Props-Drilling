77b7fd410784677a0c5d5b3a5b67c3332924d43b
Ganesh Aher <aherganesh194@gmail.com>
2022-12-03 22:03:22 +0530

    Props Drilling

---
+++ src/ComA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ComB from './ComB'
+
+const ComA = ({name}) => {
+  return (
+    <div>
+        <ComB name={name}/>
+
+
+    </div>
+  )
+}
+
+export default ComA
